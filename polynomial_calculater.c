f194c1d1f0daee5712f8859bed748313535aceff
esmanursaracbasi <40885422+esmanursaracbasi@users.noreply.github.com>
2021-02-27 13:59:54 +0300

    Update polynomial_calculater.c

--- polynomial_calculater.c
+++ polynomial_calculater.c
@@ -1,26 +1,51 @@
 #include <stdio.h>
 #include <math.h>
-int main()
-{
-  int a, b, c, d;
-  double root1, root2;
+#include <string.h>
+#include <stdlib.h>
+char** rootFinder();
 
-  printf("Enter a, b and c where a*x*x + b*x + c = 0\n");
-  scanf("%d%d%d", &a, &b, &c);
+char** rootFinder(int a, int b, int c)
+{
+  int i;
+  int d;
+  char *root1[30];
+  char *root2[30];
+  char **roots_str; 
+  roots_str = malloc(3 * sizeof(char*));
+  
+  for (int i = 0; i < 3; i++)
+    roots_str[i] = malloc((30+1) * sizeof(char));
 
   d = b*b - 4*a*c;
 
-  if (d < 0) { // complex roots, i is for iota (√-1, square root of -1)
-    printf("First root = %.2lf + i%.2lf\n", -b/(double)(2*a), sqrt(-d)/(2*a));
-    printf("Second root = %.2lf - i%.2lf\n", -b/(double)(2*a), sqrt(-d)/(2*a));
+  if (d < 0) 
+  { // complex roots, i is for iota (√-1, square root of -1)
+    snprintf(root1, 128, "%.2lf + i%.2lf", -b/(double)(2*a), sqrt(-d)/(2*a));
+    snprintf(root2, 128, "%.2lf - i%.2lf", -b/(double)(2*a), sqrt(-d)/(2*a));
+    strcpy(roots_str[0], root1);
+    strcpy(roots_str[1], root2 );
   }
   else { // real roots
-    root1 = (-b + sqrt(d))/(2*a);
-    root2 = (-b - sqrt(d))/(2*a);
-
-    printf("First root = %.2lf\n", root1);
-    printf("Second root = %.2lf\n", root2);
+    snprintf(root1, 128, "%.2lf", (-b + sqrt(d))/(2*a));
+    snprintf(root2, 128, "%.2lf", (-b - sqrt(d))/(2*a));
+    strcpy(roots_str[0], root1 );
+    strcpy(roots_str[1], root2 );
   }
-
-  return 0;
+  return roots_str;
+}
+int main(int argc, char *argv[])
+{   
+    int i;
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    int c = atoi(argv[3]);
+    char **roots_str;
+    
+    roots_str = rootFinder(a,b,c);
+    
+    for(i=0; i<2; i++)
+    {
+        printf("%s\n", roots_str[i]);
+    }
+    return 0; 
 }
